7fc4ddcecf31a29f2034b48b7fed2de6e912cd6a
Mathis Antony <sveitser@gmail.com>
2012-10-20 12:45:12 +0800

    small

--- code/mc/plots.m
+++ code/mc/plots.m
@@ -1,11 +1,13 @@
-ps = [0.3,0.31,0.32,0.5];
-st = 'rkmbgc';
+close all; clear all;
+figure;
+%ps = [0.3,0.305,0.3075,0.309,0.31,0.311,0.3125,0.315];
+ps = [0.29,0.295,0.299,0.2999,0.3,0.3001,0.301];
+hold all;
 for i=1:length(ps);
    p = ps(i);
    d=load(['p_',num2str(p),'.dat']);
    [f, x] = ecdf(d);
-   hold on;
-   plot(x,f,st(i));
+   plot(x,f);
    leg{i} = ['p = ',num2str(p)];
 end
 
