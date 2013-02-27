4cd2d64499b597c4aaee734cfd5f32f4617d259b
Mathis Antony <sveitser@gmail.com>
2013-02-27 21:05:45 +0800

    WIP: rewi

--- code/rewi/rewi.py
+++ code/rewi/rewi.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python3
 import networkx as nx
 import numpy as np
+from numpy.random import randint
+from numpy.random import rand
 
-
-# barabasi_albert_graph(n, m, seed=None)
 class System:
-    def __init__(self, generator, n, m=None):
-        self.graph = generator(n, m)
-        self.states = [np.random.randint(2) for i in range(n)]
+    def __init__(self, n, generator, genargs=None, phi=0.5, eta=0.5):
+        """
+        phi: probability of rewiring
+        eta: fraction of heterophil (type 1) agents
+        """
+        self.graph = generator(n, *genargs)
+        self.opinions = [randin(2) if for i in range(n)]
+        self.types = [0 if rand() > eta else 1 for i in range(n)]
+        self.phi = phi
     
-    def update(self, fun):
+    def update_link(self, i):
+        neis = self.graph.edge[i]
+        self.graph.remove_edge(i, neis[randint(len(neis)))
+        strangers = list(set(range(n)).difference(self.graph.edge[i] + [i]))
+        self.graph.add_edge(i, strangers[randint(len(strangers))])
+        
 
-def update(graph):
+    def update_state(self, i):
+
+
+    def update(self, fun):
+        
     
 
 if __name__ == "__main__":
     print("testing...")
+    S = System(100, nx.generators.barabasi_albert_graph, [4], 0.5, 0.1)
+
+
+    print(S.graph.degree())
